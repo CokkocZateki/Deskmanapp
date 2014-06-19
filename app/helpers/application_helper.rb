bce81ab0bb3c01900f5c645bdb695d0f88de3412
Colin Meret <colinmeret@gmail.com>
2014-06-19 12:33:11 -0400

    changed current_user method to find_by_id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,8 @@ module ApplicationHelper
 	private
 	def current_user
 		#puts "User id of session is " + session[:user_id]
-		session[:user_id] = 1
 		if session[:user_id]
-			@current_user ||= User.find(session[:user_id]) 
+			@current_user ||= User.find_by_id(session[:user_id]) 
 		else
 			nil
 		end
