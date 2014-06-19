2d36dee20a12a1af613c297504d1cbeeb506fcce
Colin Meret <colinmeret@gmail.com>
2014-06-19 12:09:09 -0400

    changed get_record function to not fail

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -3,7 +3,11 @@ module StaticHelper
     d = Shift.all.select("shift_start", "shift_end","id", "user_id")
     d = JSON.parse(d.to_json)
     d.each do |a|
-    	a["user_id"] = (User.find_by_id(a["user_id"]).try(:username) or "1")
+    	begin
+    		a["user_id"] = (User.find_by_id(a["user_id"]).try(:username) or "1")
+    	rescue ActiveRecord::RecordNotFound => e
+    		next
+    	end
     end
     d = d.to_json
     d.gsub(/shift_/, '').gsub(/user_id/, 'title').html_safe
