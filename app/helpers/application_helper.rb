1d7605c569227011a9a3f90eba2d588deaf42664
Colin Meret <colinmeret@gmail.com>
2014-06-19 12:02:11 -0400

    changed current_user function to not fail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,11 @@ module ApplicationHelper
 	private
 	def current_user
 		#puts "User id of session is " + session[:user_id]
-		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		if session[:user_id]
+			@current_user ||= User.find(session[:user_id]) 
+		else
+			nil
+		end
 	end
 
 end
